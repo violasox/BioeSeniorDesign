7ccc52e52d41b3c191ee61c0fe1bb49e3727b3a1
Soumya Vhasure <soumya.vhasure@gmail.com>
2019-03-02 02:54:21 -0500

    Fixed GUI display for phone number entry, added idle mode

--- SD_GUI_5.py
+++ SD_GUI_5.py
@@ -11,14 +11,16 @@
 import tkinter
 from tkinter import *
 import time
+from time import strftime
 import random
 from twilio.rest import Client
 import os
+import re
 from dotenv import Dotenv
 import subprocess
 import sys
 from threading import Thread
-from multiprocessing.pool import ThreadPool
+#from multiprocessing.pool import ThreadPool
 
 import RPi.GPIO as GPIO
 
@@ -27,6 +29,8 @@ class App:
         self.top = master
 
         # Parameters
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.log_path = os.path.join(self.dir_path,'app.log')
         self.canvasDim = (480,320)
         self.alarmThreshold = 2.0
         self._job = None
@@ -41,6 +45,9 @@ class App:
         ["*","0","#","D"]]
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
+        self.BUZZER = 26
+        GPIO.setup(self.BUZZER,GPIO.OUT)
+        
         self.ROW1 = 23
         self.ROW2 = 18
         self.ROW3 = 15
@@ -70,14 +77,15 @@ class App:
         #self.lowPressureLim = 3
         #self.phoneNum = '7342337513'
     
-        self.setPressurePool = ThreadPool(processes=1)
+        #self.setPressurePool = ThreadPool(processes=1)
     
-        self.setPressure = "0"
+        self.setPressure = ""
+        self.newPressureDigit = ""
         self.currentPressure = DoubleVar()
         self.highPressureLim = self.currentPressure.get() + self.alarmThreshold
         self.lowPressureLim = self.currentPressure.get() - self.alarmThreshold
-        self.phoneNumArray = [0,0,0,0,0,0,0,0,0,0]
-        self.phoneNumStr = ""
+        self.phoneNum = ""
+        self.newPhoneDigit = ""
 
 
         # widgets
@@ -97,7 +105,7 @@ class App:
 
         # Enter phone number
         self.L_enterPhoneNum = Label(top, text = "Enter Phone Number", font=("Helvetica", 20))
-        self.T_enterPhoneNum = Text(top, height=1,width=12,font=("Helvetica", 20))
+        self.L_enteredPhoneNum = Label(top, text = self.phoneNum, font=("Helvetica", 20))
         self.B_enterPhoneNum = Button(top, text = "Enter",bd=5, relief=RAISED, padx=15,pady=5,command=self.pressureDisplayPage)
         self.B_enterPhoneNum.config(font=("Helvetica", 15))
 
@@ -109,7 +117,7 @@ class App:
 
         # Calibration Page 1
         self.L_pressureSet = Label(top, text = "1. Enter pressure set in bubble CPAP system", font=("Helvetica", 20))
-        self.T_pressureSet = Text(top, height=1,width=3,font=("Helvetica", 20))
+        self.L_pressureSetVal = Label(top, text=self.setPressure, font=("Helvetica", 20))
         self.B_pressureSet = Button(top, text = "Enter",bd=5, relief=RAISED, padx=15,pady=5,command=self.testCalibrationLow)
         self.B_pressureSet.config(font=("Helvetica", 15))
 
@@ -151,7 +159,7 @@ class App:
         self.maxPressureV = 4.0;
         self.zeroPressure = 0.0;
         self.maxPressure = 35.56;
-        self.sendTextDelay = 15; # send text every __ seconds
+        self.sendTextDelay = 30; # send text every __ seconds
         self.timeSent = 0;
         self.timeElapsed = 0;
 
@@ -177,6 +185,15 @@ class App:
         self.B_monitor.grid(row=2,column=0,padx=70,sticky=E)
         
         self.startCalibration = False
+        self.pressureIsSet = False
+        self.startEnterPhoneNum = False
+        self.phoneNumIsSet = False
+        
+        self.textAcknowledged = False
+        self.startIdleMode = False
+        self.idleModeDuration = 65
+        self.idleModeTimeStarted = 0
+        self.idleModeTimeElapsed = 0
         
 
         # Uncomment for testing purposes
@@ -189,14 +206,15 @@ class App:
 
 
     def getNewData(self):
-        return random.uniform(1.0,10.0)
+        ## TO USE WITH COMPUITER FOR DEBUGGING ONLY
+        #return random.uniform(1.0,10.0)
 
-        ## TO USE WITH PRESSURE SENSOR
-        #curPressureTicks = self.readadc(self.potentiometer_adc, self.SPICLK, self.SPIMOSI, self.SPIMISO, self.SPICS)
-        #curPressureV = (self.maxV*curPressureTicks) / self.numTicks;
-        #conversionV2P = (self.maxPressure - self.zeroPressure)/(self.maxPressureV - self.zeroPressureV);
-        #curPressure = (curPressureV-self.zeroPressureV)*conversionV2P;
-        #return curPressure
+        # TO USE WITH PRESSURE SENSOR
+        curPressureTicks = self.readadc(self.potentiometer_adc, self.SPICLK, self.SPIMOSI, self.SPIMISO, self.SPICS)
+        curPressureV = (self.maxV*curPressureTicks) / self.numTicks;
+        conversionV2P = (self.maxPressure - self.zeroPressure)/(self.maxPressureV - self.zeroPressureV);
+        curPressure = (curPressureV-self.zeroPressureV)*conversionV2P;
+        return curPressure
 
     # Credit to Adafruit for this function
     # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
@@ -235,7 +253,7 @@ class App:
             return adcout
         
     def numWriter(self):
-        print('Got here 3')
+        #print('Got to numWriter')
         counter = 0
         num = ""
         while counter == 0:
@@ -247,14 +265,39 @@ class App:
                         num = self.MATRIX[i][j]
                         counter = 1
                         #print(MATRIX[i][j])
-                        time.sleep(0.5)
+                        time.sleep(0.1)
                         while(GPIO.input(self.ROW[i])) == 0:
                             pass
                 GPIO.output(self.COL[j],1)
     
-        print('Got here 4')
+        #print('num is ', num)
         return num
-
+    
+    def buzz(self):
+        GPIO.output(self.BUZZER,GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(self.BUZZER,GPIO.LOW)
+        time.sleep(0.5)
+    
+    def checkAlertAcknowledged(self):
+        #checkTimestamp = "[2019-03-02 02:19:28]"
+        checkTimestamp = strftime('[%Y-%m-%d %H:%M:%S]')
+        checkMin = checkTimestamp[15:17]
+        #checkSec = checkTimestamp[19:21]
+        #print(checkMin)
+        #input("Press enter to continue...")
+        with open(self.log_path, "r") as log_file:
+            lines = log_file.read().splitlines()
+            last_line = lines[-1]
+        lastLogMin = last_line[15:17]
+        #lastLogSec = last_line[19:21]
+        #print(lastLogMin)
+        #input("Press enter to continue")
+        #print(str(int(checkMin) - int(lastLogMin)))
+        #print(str(int(checkSec) - int(lastLogSec)))
+        if (int(checkMin) - int(lastLogMin) == 0 or int(checkMin) - int(lastLogMin) == -59):
+            if "200 OK" in last_line:
+                self.textAcknowledged = True
 
     def endProcess(self):
         if self._job is not None:
@@ -276,31 +319,39 @@ class App:
         self.B_calibrate.grid(row=2,column=0,padx=70,sticky=W)
         self.B_monitor.grid(row=2,column=0,padx=70,sticky=E)
     
-    
-    
     def enterPhoneNumPage(self):
-        self.clear();
+        self.endProcess()
+        self.clear()
         self.L_oxydas.grid(row=0,padx=175,pady=25,sticky=N)
         self.L_oxydas.config(fg=self.blue,bg="white")
 
         self.L_enterPhoneNum.grid(row=1,pady=20)
-        #self.E_enterPhoneNum.grid(row=2)
+        #self.L_enteredPhoneNum.grid(row=2)
+        #print("showing phone num")
         self.B_enterPhoneNum.grid(row=3,pady=20)
         
-        tempPhoneStr = ""
-        for x in range(10):
-            self.phoneNumArray[x]=self.numWriter()
-            for k in range(x+1):
-                tempPhoneStr += self.phoneNumArray[k]
-            self.phoneNumStr = tempPhoneStr
-            #print(tempPhoneStr)
-            tempPhoneStr = ""
+        # If phone number has not been entered yet
+        if (not self.phoneNumIsSet):
+            numDigits = len(self.phoneNum)
+            # Iteration 1 to display everything else on the screen
+            if (not self.startEnterPhoneNum):
+                self.startEnterPhoneNum = True
+            # Iterations 2-10
+            elif (numDigits < 10):
+                self.newPhoneDigit = self.numWriter()
+            # Iterations 11+
+            else:
+                self.newPhoneDigit = ""
+                self.phoneNumIsSet = True
+            self.phoneNum = "{}{}".format(self.phoneNum, self.newPhoneDigit)
+            self.L_enteredPhoneNum.config(text = self.phoneNum)
+            self.L_enteredPhoneNum.grid(row=2)
+               
+        else:
+            self.L_enteredPhoneNum.config(text = self.phoneNum)
+            self.L_enteredPhoneNum.grid(row=2)
         
-        self.T_enterPhoneNum.insert(INSERT, self.phoneNumStr)
-        self.T_enterPhoneNum.grid(row=2,column=0)
-        
-        #print(self.phoneNumStr)
-        #print(type(self.phoneNumStr))
+        self._job = self.top.after(1000,self.enterPhoneNumPage)
 
     def getStatus(self, currentPressure, lowPressureLim, highPressureLim):
         # Cannula in
@@ -309,7 +360,7 @@ class App:
         # Cannula out
         elif currentPressure < lowPressureLim:
             return 0
-        # Tubing kinked
+        # High pressure
         else:
             return 2
 
@@ -321,7 +372,16 @@ class App:
                      #status_callback="https://postb.in/b/GbJzpb3m")
         #time.sleep(15) # wait time in seconds
         #self.textTimer == 0
-
+    
+    def enterIdleMode(self):
+        # if idle mode has been entered, idle mode has started and the text has been acknowledged
+        self.startIdleMode = True
+        self.textAcknowledged = True
+        time.sleep(self.idleModeDuration)
+        # at the end of the idle mode period, reset startIdleMode and textAcknowledged
+        self.startIdleMode = False
+        self.textAcknowledged = False
+    
     def pressureGuiUpdate(self):
         self.currentPressure = self.getNewData()
         status = self.getStatus(self.currentPressure, self.lowPressureLim, self.highPressureLim)
@@ -346,18 +406,63 @@ class App:
                 wid.config(bg=self.green)
             L_status = Label(top, text = "Cannula In",font=("Helvetica", 30))
             L_status.config(bg=self.green)
+        # when cannula comes out
         elif (status == 2):
-            if (self.timeSent == 0 or self.timeElapsed > self.sendTextDelay):
-                #print("Text sent")
-                self.sendText()
-                self.timeSent = time.time();
+            # check if the nurse acknowledged the message only if not already in idle mode
+            if (self.startIdleMode is False):
+                self.checkAlertAcknowledged()
+            print("text acknowledged is ", str(self.textAcknowledged))
+            # if nurse hasn't acknowledged the message
+            if (self.textAcknowledged is False):
+                self.idleModeTimeStarted = 0
+                self.idleModeTimeElapsed = 0
+                print("Text not seen yet, need to start buzzing")
+                # start buzzing
+                BuzzThread = Thread(target=self.buzz)
+                BuzzThread.start()
+                # if a text hasn't been sent or it has been a minute, send a text
+                if (self.timeSent == 0 or self.timeElapsed > self.sendTextDelay):
+                    print("Text sent")
+                    self.sendText()
+                    self.timeSent = time.time();
+                else:
+                    #print("Passed")
+                    pass
+            # if a nurse has acknowledged the message
             else:
-                #print("Passed")
-                pass
+                #print("Text has been seen")
+                # go into idle mode if it hasn't already happened
+                if (self.idleModeTimeStarted == 0):
+                    #self.startIdleMode = True
+                    print("STAAAAAART")
+                    self.startIdleMode = True
+                    self.textAcknowledged = True
+                    self.idleModeTimeStarted = time.time()
+                elif (self.idleModeTimeElapsed < self.idleModeDuration):
+                    print("In IDLE MODE: ", str(self.idleModeTimeElapsed))
+                elif (self.idleModeTimeElapsed >= self.idleModeDuration):
+                    print("Idle mode complete")
+                    self.startIdleMode = False
+                    self.textAcknowledged = False
+                    #self.idleModeTimeStarted = 0;
+                
+                
+                #self.idleModeDuration = 30
+                #self.idleModeTimeStarted = 0
+                #self.idleModeTimeElapsed = 0
+                #if (self.startIdleMode is False):
+                #    print('start idle mode is', self.startIdleMode)
+                #    IdleThread = Thread(target=self.enterIdleMode)
+                #    IdleThread.start()
+                ## if idle mode has already started, don't go into it again
+                #else:
+                #    print("IN IDLE MODE ", self.startIdleMode)
+                #    pass
             for wid in myWidgets:
                 wid.config(bg=self.red)
             L_status = Label(top, text = "Low Pressure",font=("Helvetica", 30))
             L_status.config(bg=self.red)
+        # when there is high pressure (status = 3)
         else:
             for wid in myWidgets:
                 wid.config(bg="orange")
@@ -365,16 +470,18 @@ class App:
             L_status.config(bg="orange")
 
         self.timeElapsed = time.time() - self.timeSent
+        self.idleModeTimeElapsed = time.time() - self.idleModeTimeStarted
 
         L_status.grid_remove()
         L_status.grid(row=3)
 
     def pressureDisplayPage(self):
-        envPhoneNum = "+1" + self.phoneNumStr
-        #print(type(envPhoneNum))
-        #os.environ['RECV_NUMBER']=envPhoneNum
-
+        self.endProcess()
         self.clear()
+        
+        envPhoneNum = "+1" + self.phoneNum
+        ##print(type(envPhoneNum))
+        #os.environ['RECV_NUMBER']=envPhoneNum
         self.L_oxydas.grid(row=0,padx=175,pady=25,sticky=N)
 
         self.pressureGuiUpdate();
@@ -420,43 +527,53 @@ class App:
         L_status.grid(row=3)
 
     def calibrationPage1(self):
+        self.endProcess()
         self.clear()
         self.L_oxydas.grid(row=0,padx=175,pady=25,sticky=N)
         self.L_oxydas.config(bg="white")
         self.L_units.config(bg="white")
-
+        
         self.L_pressureSet.grid(row=1, pady=20)
-
+        self.L_pressureSetVal.grid(row=2,column=0)
         self.L_units.grid(row=2,column=0,padx=120,sticky=E)
         self.B_pressureSet.grid(row=3,pady=30)
         
-        
-        
-        #async_result = self.setPressurePool.apply_async(self.numWriter)
-        #self.setPressure = async_result.get()
-        #print(self.setPressure)
-
-        if (not self.startCalibration):
-            self.startCalibration = True
-            newDigit = ""
-            print('Got here')
+        # If pressure has not been set yet
+        if (not self.pressureIsSet):
+            numDigits = len(self.setPressure)
+            # Iteration 1
+            if (not self.startCalibration):
+                self.startCalibration = True
+                print('Got here')
+            # Iteration 2 - until the first number is pressed
+            elif (numDigits == 0):
+                self.newPressureDigit= self.numWriter()
+                self.setPressure = "{}{}".format(self.setPressure, self.newPressureDigit)
+                self.L_pressureSetVal.config(text = self.setPressure)
+                if (self.newPressureDigit != "1"):
+                    self.pressureIsSet = True
+                    print('Pressure is set!')
+                else:
+                    pass
+            # If first digit entered is 1, the second digit entered is only allowed to be 0
+            else:
+                self.newPressureDigit = self.numWriter()
+                if (self.setPressure != "1" or self.newPressureDigit != "0"):
+                    self.newPressureDigit = ""
+                    print("Only 0 allowed after 1")
+                else:
+                    self.pressureIsSet = True
+                    self.setPressure = "{}{}".format(self.setPressure, self.newPressureDigit)
+                    self.L_pressureSetVal.config(text = self.setPressure)
+                    print('Pressure is set!')
         else:
-            newDigit= self.numWriter()
-            print('Got here 2')
-        # self.setPressure = self.numWriter()
-        #newDigit = self.numWriter()
-        self.setPressure = "{}{}".format(self.setPressure, newDigit)
-        L_pressureSet = Label(top,text=self.setPressure)
-        L_pressureSet.grid(row=2,column=0)
-        self.top.after(1000, self.calibrationPage1)
-            
-        #print(self.setPressure)
-
-        #self.T_pressureSet.insert(INSERT, self.setPressure)
-        #self.T_pressureSet.grid(row=2,column=0)
+            pass
+        
+        self._job = self.top.after(1000, self.calibrationPage1)
         
 
     def testCalibrationLow(self):
+        self.endProcess()
         self.clear()
         self.L_oxydas.grid(row=0,padx=175,pady=25,sticky=N)
 
@@ -481,7 +598,7 @@ class App:
         self.L_testHigh.grid(row=4,padx=40,pady=30,sticky=SW)
         self.B_testHigh.grid(row=4,padx=50,pady=35,sticky=E)
 
-
+        self._job = self.top.after(1000,self.testCalibrationHigh)
 
 
 top = Tk()
